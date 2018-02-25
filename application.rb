5c96ce1c6f790f834dc3f0941972832220fbcff9
Jen Poole <contact.vandepool@gmail.com>
2018-02-25 01:18:07 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,10 @@
 class Application
-
+  # create a new class that responds to a single method (#call)
+  # All this method needs to do is return a response which consists of the status code, any headers, and the body
   def call(env)
-    resp = Rack::Response.new
+    resp = Rack::Response.new # This can be done using the Rack::Response object
     resp.write "Hello, World"
     resp.finish
   end
 
 end
-
