c0726c3cb34980594dc84c3d8f41c585d329cc73
michael hohl <mhohl@rocketmail.com>
2018-05-08 18:16:12 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is h o h l t h i n g"
     resp.finish
   end
 
 end
-
