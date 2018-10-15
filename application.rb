2b25e75bf37af73267dbdd4048bd070cfda50354
kenkuts <kennethchoi19@gmail.com>
2018-10-15 16:15:16 -0400

    Rack Practice

--- application.rb
+++ application.rb
@@ -1,8 +1,27 @@
 class Application
 
+  @@items = ["apple", "banana", "cherry"]
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is Kenneth R. Kutschera"
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_item = req.params['q']
+
+      if @@items.include?(search_item)
+        resp.write "#{search_item} is included!"
+      else
+        resp.write "#{search_item} is not included!"
+      end
+
+    else
+      resp.write "Path not found!"
+    end
+
     resp.finish
   end
 
